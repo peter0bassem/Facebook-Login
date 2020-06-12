a122d6a34e86559b47f4bb6dc051118e2dbad4dd
peter0bassem <pero.bassem@gmail.com>
2020-06-12 21:34:27 +0200

    facebook login storyboard

--- Facebook_Login/ViewController.swift
+++ Facebook_Login/ViewController.swift
@@ -15,22 +15,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        if let token = AccessToken.current, !token.isExpired {
-            let token = token.tokenString
-            
-            //"email, name, picture.type(large), gender, birthday, phone"
-            
-            /// profile image sizes: small, normal, album, large, square
-            
-            getFacebookUser(token)
-            
-        } else {
-            let loginButton = FBLoginButton()
-            loginButton.center = view.center
-            loginButton.delegate = self
-            loginButton.permissions = ["public_profile", "email", "user_gender", "user_birthday"]
-            view.addSubview(loginButton)
-        }
+//        if let token = AccessToken.current, !token.isExpired {
+//            let token = token.tokenString
+//            
+//            //"email, name, picture.type(large), gender, birthday, phone"
+//            
+//            /// profile image sizes: small, normal, album, large, square
+//            
+//            getFacebookUser(token)
+//            
+//        } else {
+//            let loginButton = FBLoginButton()
+//            loginButton.center = view.center
+//            loginButton.delegate = self
+//            loginButton.permissions = ["public_profile", "email", "user_gender", "user_birthday"]
+//            view.addSubview(loginButton)
+//        }
     }
     
     func getFacebookUser(_ token: String?) {
@@ -43,17 +43,34 @@ class ViewController: UIViewController {
             print("\(result)")
         }
     }
+    
+    @IBAction func onFacebookLoginButtonPressed(_ sender: UIButton) {
+        let facebookLoginManager = LoginManager()
+        facebookLoginManager.logIn(permissions: ["public_profile", "email", "user_gender", "user_birthday"], from: self) { [weak self] (result, error) in
+            if let error = error {
+                print("Faile to login with facebook:", error)
+                return
+            }
+            guard let result = result else { return }
+            if result.isCancelled {
+                print("User cancelled facebook login")
+                return
+            }
+            let token = result.token?.tokenString
+            self?.getFacebookUser(token)
+        }
+    }
 }
 
-extension ViewController: LoginButtonDelegate {
-    
-    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
-        let token = result?.token?.tokenString
-        
-        getFacebookUser(token)
-    }
-    
-    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
-        print("logged out successfully")
-    }
-}
+//extension ViewController: LoginButtonDelegate {
+//    
+//    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+//        let token = result?.token?.tokenString
+//        
+//        getFacebookUser(token)
+//    }
+//    
+//    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+//        print("logged out successfully")
+//    }
+//}
